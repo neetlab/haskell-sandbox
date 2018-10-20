75517bbfcfe67fc0b60bf271f59a2916cbf8aabc
neet <n33t5hin@gmail.com>
2018-10-20 21:43:59 +0900

    Add list methods

--- app/List.hs
+++ app/List.hs
@@ -1,1 +1,15 @@
 module List where
+
+import Data.List
+
+-- Adding
+a = 1 : [2, 3] -- [1, 2, 3]
+
+-- Getting
+b = [1, 2, 3] !! 2 -- 3
+
+-- Sorting
+c = sort [3, 2, 1] -- [1, 2, 3]
+
+-- Includes
+d = 4 `elem` [3, 2, 1] -- False
