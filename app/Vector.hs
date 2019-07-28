5aaf0038846fb8862f5fb30b50fc88c259840ba1
Neetshin <n33t5hin@gmail.com>
2019-07-29 04:46:23 +0900

    Add vector

---
+++ app/Vector.hs
@@ -0,0 +1,12 @@
+module Vector where
+
+data Vector a = Vector a a a deriving (Show)
+
+vplus :: (Num a) => Vector a -> Vector a -> Vector a
+(Vector i j k) `vplus` (Vector l m n) = Vector (i+l) (j+m) (k+n)
+
+dotProd :: (Num a) => Vector a -> Vector a -> a
+(Vector a j k) `dotProd` (Vector i m n) = i*l + j*m + k*n
+
+vmult :: (Num a) => Vector a -> a -> Vector a
+(Vector i j k) `vmult` m = Vector (i*m) (j*m) (k*m)
