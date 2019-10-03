df74640d6d5c0cf18ceb3966d77ca701ca104618
Neetshin <n33t5hin@gmail.com>
2019-10-03 16:06:55 +0900

    applicative則

--- app/Applicative/applicative.hs
+++ app/Applicative/applicative.hs
@@ -28,4 +28,32 @@ pure (2:) <*> [] -- Just [2]
 f <$> Just 3       -- これを使う方法が無い
 f <*> Just 3 <*> g -- applicativeなら `g` で使えばいい
 
+-- Arrayにもつかえるよ
+(:) <$> Just 2 <*> Just [] -- Just [2]
+filter (>50) $ (*) <$> [2,5,10] <*> [8,10,11]
 
+-- I/O Monad
+action :: IO String
+action = (++) <$> getLine <*> getLine
+
+pure "foo" :: [String]     -- ["foo"]
+pure "foo" :: Maybe String -- Just "foo"
+pure "foo"                 -- "foo" (->らしい)
+
+{-
+action = pure (++) <*> getLine <*> getLine
+action = return (++) <*> getLine <*> getLine
+action = do
+  a <- getLine
+  b <- getLine
+  return f a b
+-}
+
+{-
+Applicativeとして使いづらいからという理由で[]とは別の
+zip用途のZipListというのがある。
+-}
+getZipList $ (+) <$> ZipList [1,2,3] <*> ZipList [100,100,100]
+-- [101,102,103]
+(+) <$> [1,2,3] <*> [100,100,100]
+-- [101,101,101,102,102,102,103,103,103]
--- app/Functor/laws.hs
+++ app/Functor/laws.hs
@@ -4,11 +4,12 @@ id <$> [1, 2, 3]
 id <$> Right "right"
 id <$> Left  "wrong"
 
--- ファンクター則 2
+-- ファンクター則 2 呼び出し順を影響しない的な
 f :: Num x => x -> x
 f = (+ 1)
 g :: Num x => x -> x
 g = (+ 2)
+
 f . g <$> Just 0 == f <$> g <$> Just 0
 
 -- 2引数関数で使うと...
