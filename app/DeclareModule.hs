19df3e885d0453f8d2c9423a4b5fe08c97b43f17
Neetshin <n33t5hin@gmail.com>
2019-11-16 15:01:23 +0900

    a

--- app/DeclareModule.hs
+++
@@ -1,8 +0,0 @@
-module DeclareModule where
-
--- module <モジュール名> where でファイル内の関数や型をモジュールとして定義可能
--- module <モジュール名> (エクスポートする関数, ...) where でエクスポートする関数や型を制限可能
--- <モジュール名> はファイル名と同じである必要がある
-
--- import <Hierarchy.Module> <(Function/Name)> [qualified <Module> as name]
--- import qualified Function (myFunc) as func
